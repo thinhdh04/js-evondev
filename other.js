c414f2357df042031b78b57c480c729f8726f405
thinhdh04 <tdh855513@gmail.com>
2024-12-08 01:03:28 +0700

    chuong-2

---
+++ other.js
@@ -0,0 +1,15 @@
+// undefined -> khai bao nhung chua gan gia tri
+let a;
+console.log(a); // undefined
+let b;
+console.log(b); 
+b = 10;
+console.log(b); 
+const c = "string";
+// null -> nothing 
+let d = null;
+console.log(d); 
+// Boolean -> true or false 
+// falsy values or truthy values
+// falsy values: "", 0, flase, undefined, null 
+// truthy values: "abc", 1, true, 100, 1000
